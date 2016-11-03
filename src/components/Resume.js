8e26da7e830bfea9cb5f6b893f8a9d2725bfb900
Clark Sanford <clrksanford@gmail.com>
2016-11-03 17:52:55 -0500

    Working on resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import resumePic from '../../public/images/clark-sanford-resume.jpg';
 
 class Resume extends Component {
   render() {
     return(
-      <div className="pageContent">
+      <div className='pageContent'>
         <h3>Resume</h3>
+        <a href={resumePic} download>Download</a>
       </div>
     );
   }

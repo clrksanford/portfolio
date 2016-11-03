400d1f8d097912656e5747dbc0f9499e2e1339e3
Clark Sanford <clrksanford@gmail.com>
2016-11-03 15:57:40 -0500

    Added images, started constructing projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,44 @@
 import React, {Component} from 'react';
+import danceFinder from '../../public/images/dance-finder.jpg';
+import dateNight from '../../public/images/date-night.jpg';
 
 class Projects extends Component {
   render() {
     return(
-      <div className="pageContent">
-        <h3>Projects</h3>
+      <div className='pageContent'>
+        <div className='pageHeader'>
+          <h3>Projects</h3>
+        </div>
+        <div className='pageMain'>
+          <div className='row'>
+            <div className='column'>
+              <h4>Dance Finder</h4>
+                <a href='https://clrksanford.github.io/dance-finder' target='_blank'>
+                  <img src={danceFinder}/>
+                </a>
+                <a href='https://github.com/clrksanford/dance-finder'  target='_blank'>GitHub Link</a>
+            </div>
+            <div className='column'>
+              <h4>Date Night</h4>
+                <a href='https://clrksanford.github.io/date-night' target='_blank'>
+                  <img src={dateNight}/>
+                </a>
+                <a href='https://github.com/clrksanford/date-night'  target='_blank'>GitHub Link</a>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='column'>
+            </div>
+            <div className='column'>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='column'>
+            </div>
+            <div className='column'>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }

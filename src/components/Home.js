a573caca5f0901071f7852aeccd9133f6334bdfe
Clark Sanford <clrksanford@gmail.com>
2016-11-08 09:35:14 -0600

    More failed attempts to animate homepage info

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Keyframes, Frame} from 'react-keyframes';
 import headshot from '../../public/images/headshot.jpg';
 
 class Home extends Component {
@@ -7,26 +8,23 @@ class Home extends Component {
       <div className="pageContent">
         <div className='pageHeader'>
           <h1>Portfolio of Clark Sanford</h1>
-          <h2 id='homeSubtitle'>Full-Stack Web Developer Based in ATX</h2>
+          <h2 id='homeSubtitle' className='fadeIn'>Full-Stack Web Developer Based in Austin, TX</h2>
         </div>
         <div className='row clearfix' id='aboutContainer'>
           <img src={headshot} alt='headshot' id='headshot'/>
           <div id='aboutParagraph'>
-            <p id='p1'>
-            Literature major, world traveler
-            </p>
-            <p id='p2'>
-            Highly independent critical thinker
-            </p>
-            <p id='p3'>
-            Process-oriented and creative
-            </p>
-            <p id='p4'>
-            Excellent communicator
-            </p>
-            <p id='p5'>
-            Innovative problem solver
-            </p>
+          <Keyframes>
+            <Frame duration={500}>This</Frame>
+            <Frame duration={500}>This is</Frame>
+            <Frame duration={500}>This is <em>animated</em>.</Frame>
+          </Keyframes>
+            <ul>
+              <li className='fadeIn'>Literature major, world traveler</li>
+              <li className='fadeIn'>Highly independent critical thinker</li>
+              <li className='fadeIn'>Process-oriented and creative</li>
+              <li className='fadeIn'>Excellent communicator</li>
+              <li className='fadeIn'>Innovative problem solver</li>
+            </ul>
           </div>
         </div>
       </div>

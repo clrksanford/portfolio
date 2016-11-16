2a236e9efc8fc91071a45a149f1a8a3a26266403
Clark Sanford <clrksanford@gmail.com>
2016-11-16 09:31:25 -0600

    Remove animations from home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import headshot from '../../public/images/headshot.jpg';
 
 class Home extends Component {
@@ -13,47 +12,11 @@ class Home extends Component {
         <div className='row clearfix' id='aboutContainer'>
           <img src={headshot} alt='headshot' id='headshot'/>
           <div id='aboutParagraph'>
-          <ReactCSSTransitionGroup
-           transitionName="first"
-           transitionAppear={true}
-           transitionAppearTimeout={2000}
-           transitionEnter={false}
-           transitionLeave={false}>
            <p id='p1'>Literature major, world traveler</p>
-          </ReactCSSTransitionGroup>
-          <ReactCSSTransitionGroup
-           transitionName="first"
-           transitionAppear={true}
-           transitionAppearTimeout={10000}
-           transitionDelay={3000}
-           transitionEnter={false}
-           transitionLeave={false}>
            <p id='p2'>Highly independent critical thinker</p>
-          </ReactCSSTransitionGroup>
-          <ReactCSSTransitionGroup
-           transitionName="third"
-           transitionAppear={true}
-           transitionAppearTimeout={2000}
-           transitionEnter={false}
-           transitionLeave={false}>
            <p id='p3'>Process-oriented and creative</p>
-          </ReactCSSTransitionGroup>
-          <ReactCSSTransitionGroup
-           transitionName="fourth"
-           transitionAppear={true}
-           transitionAppearTimeout={2000}
-           transitionEnter={false}
-           transitionLeave={false}>
            <p id='p4'>Excellent communicator</p>
-          </ReactCSSTransitionGroup>
-          <ReactCSSTransitionGroup
-           transitionName="last"
-           transitionAppear={true}
-           transitionAppearTimeout={2000}
-           transitionEnter={false}
-           transitionLeave={false}>
            <p id='p5'>Innovative problem solver</p>
-          </ReactCSSTransitionGroup>
           </div>
         </div>
       </div>

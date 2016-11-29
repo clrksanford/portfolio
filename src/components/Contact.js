69185b09d7a252c645d9c21e0605411c26ef7500
Clark Sanford <clrksanford@gmail.com>
2016-11-29 10:07:20 -0600

    Add more contact tiles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,21 +5,47 @@ class Contact extends Component {
     return(
       <div className='pageContent'>
         <div className='pageHeader'>
-          <h3>Contact</h3>
+          <h3>Get In Touch</h3>
         </div>
         <div className='pageMain'>
           <div className='row flexContainerRow'>
             <div className='contactTile'>
-              <i className="fa fa-envelope contactPageIcon" aria-hidden="true"></i>
-              <p>clrksanford@gmail.com</p>
+              <a href='mailto:clrksanford@gmail.com'>
+                <i className="fa fa-envelope contactPageIcon" aria-hidden="true"></i>
+                <p>clrksanford@gmail.com</p>
+              </a>
             </div>
             <div className='contactTile'>
               <i className="fa fa-map-marker contactPageIcon" aria-hidden="true"></i>
               <p>Austin, TX</p>
             </div>
+
+              <div className='contactTile'>
+                <a href='https://linkedin.com/in/clrksanford'>
+                  <i className="fa fa-linkedin contactPageIcon" aria-hidden="true"></i>
+                </a>
+              </div>
+          </div>
+        </div>
+        <div className='pageHeader'>
+          <h3>See What I've Been Up To</h3>
+        </div>
+        <div className='pageMain'>
+          <div className='row flexContainerRow'>
             <div className='contactTile'>
-              <i className="fa fa-linkedin contactPageIcon" aria-hidden="true"></i>
-              <p>linkedin.com/in/clrksanford</p>
+              <a href='https://github.com/clrksanford' target='_blank'>
+                <i className="fa fa-github contactPageIcon" aria-hidden="true"></i>
+              </a>
+            </div>
+            <div className='contactTile'>
+              <a href='https://twitter.com/clrksanford' target='_blank'>
+                <i className="fa fa-twitter contactPageIcon" aria-hidden="true"></i>
+              </a>
+            </div>
+            <div className='contactTile'>
+              <a href='https://medium.com/@clrksanford' target='_blank'>
+                <i className="fa fa-medium contactPageIcon" aria-hidden="true"></i>
+              </a>
             </div>
           </div>
         </div>
